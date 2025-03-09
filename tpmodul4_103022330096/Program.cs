d86743c31d13b3c702c452afc93752780d1b034f
RakaValrizqy <pendragonqword@gmail.com>
2025-03-09 21:42:55 +0700

    Menambahkan implementasi dengan state-based construction

--- tpmodul4_103022330096/Program.cs
+++ tpmodul4_103022330096/Program.cs
@@ -13,11 +13,18 @@ class KodePos
         return kodePos[(int)kelurahan];
     }
 }
+class DoorMachine
+{
+    public enum State { Terkunci, Terbuka, Keluar };
+
+    
+}
 
 class Program
 {
     static void Main()
     {
+        //table driven
         Console.Write("Masukkan nama kelurahan: ");
         string input = Console.ReadLine();
 
@@ -25,5 +32,47 @@ class Program
 
         int kodePos = KodePos.GetKodePos(kelurahan);
         Console.WriteLine($"Kode Pos untuk {kelurahan}: {kodePos}");
+
+        //state based
+        DoorMachine.State state = DoorMachine.State.Terkunci;
+        string[] screenName = { "Terkunci", "Terbuka", "Exit" };
+        while (state != DoorMachine.State.Keluar)
+        {
+            Console.WriteLine(screenName[(int)state] + " Screen");
+            Console.Write("Enter Command: ");
+
+            string command = Console.ReadLine();
+
+            switch (state) 
+            {
+                case DoorMachine.State.Terkunci:
+                    if (command == "BukaPintu")
+                    {
+                        state = DoorMachine.State.Terbuka;
+                    } else if (command == "KunciPintu")
+                    {
+                        state = DoorMachine.State.Terkunci;
+                    } else if (command == "Keluar")
+                    {
+                        state = DoorMachine.State.Keluar;
+                    }
+                    break;
+                case DoorMachine.State.Terbuka:
+                    if (command == "BukaPintu")
+                    {
+                        state = DoorMachine.State.Terbuka;
+                    }
+                    else if (command == "KunciPintu")
+                    {
+                        state = DoorMachine.State.Terkunci;
+                    }
+                    else if (command == "Keluar")
+                    {
+                        state = DoorMachine.State.Keluar;
+                    }
+                    break;
+            }
+        }
+        Console.WriteLine(screenName[(int)state] + " Screen");
     }
 }
